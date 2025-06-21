309ec8d5df8de1c2a2ff6a2c9e16a55625b1db44
gemlibra <93558651+gemlibra@users.noreply.github.com>
2025-06-20 22:43:25 -0400

    Add files via upload

---
+++ js_module4_solution.js
@@ -0,0 +1,26 @@
+
+
+(function() {
+    var nameArray = [{
+    name: 'John', 
+    name: 'James', 
+    name: 'Jennifer', 
+    name: 'Gift',
+    name: 'Xtian',
+    name: 'jasmine',
+    name: 'Zarah',
+    name: 'Clara',
+    name: 'jaspin'
+    }];
+
+    for ( i = 0; i < nameArray.length;  i++) {
+        
+        var firstLetter = nameArray[i].charAt(0).toLowerCase();
+
+        if (firstLetter = 'J' || 'j') {
+        console.log(`Hello  + ${nameArray.name}`);
+        } else {
+        console.log(`Goodbye  + ${arrayName.name}`);
+     };
+  };
+})();
